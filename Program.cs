f72c4bc24416e25593e2ae1d274ee4acdbf1a9c9
Saiaug <118536963+Saiaug@users.noreply.github.com>
2022-11-22 14:22:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using dotnetclass;
+
+namespace ConsoleApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo ob = new Demo();
+            ob.message();
+            Console.Read();
+        }
+    }
+}
